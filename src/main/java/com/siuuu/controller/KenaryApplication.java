70c565bf539ed72accdc3c57c34961d43d44feff
Marcelo Martínez <marcelo2m1097@gmail.com>
2019-10-13 18:50:26 -0600

    Class name was changed and also the class was clean up.

--- src/main/java/com/siuuu/controller/KenaryApplication.java
+++ src/main/java/com/siuuu/controller/KenaryApplication.java
@@ -3,15 +3,13 @@ package com.siuuu.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
-public class MainController {
+public class KenaryApplication {
     //This function inits the project
     public static void main(String args[]){
-        SpringApplication.run(MainController.class, args);
+        SpringApplication.run(KenaryApplication.class, args);
     }
     //This function inits the project
 }
