be40397a12ef4ccd99f04b9f149149cb6d4f00ae
Marcelo Martínez <marcelo2m1097@gmail.com>
2019-10-16 20:50:43 -0600

    Class was rename.

--- src/main/java/com/siuuu/converter/UserConverter.java
+++ src/main/java/com/siuuu/converter/UserConverter.java
@@ -1,15 +1,15 @@
 package com.siuuu.converter;
 
 import com.siuuu.domain.User;
-import com.siuuu.model.ModelUser;
+import com.siuuu.model.UserModel;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("userConverter")
 public class UserConverter {
-    public ModelUser convertUser(User user){
-        ModelUser modelUser = new ModelUser(user.getcUser(),user.getuPhoto(),user.getuFirstNames(),user.getuLastNames(),user.getuUsername(),
+    public UserModel convertUser(User user){
+        UserModel userModel = new UserModel(user.getcUser(),user.getuPhoto(),user.getuFirstNames(),user.getuLastNames(),user.getuUsername(),
                 user.getuPassword(),user.getuBirthday(),user.getuGender(),user.getuCountry(),user.getuInstitution(), user.getuDegree(),
                 user.getuCareer(),user.getuType());
-        return modelUser;
+        return userModel;
     }
 }
