ccb0497f0295711a79ea95b0b0546d7713a7dcb1
Marcelo Martínez <marcelo2m1097@gmail.com>
2019-11-18 17:34:05 -0600

    New implementation. API returns a list with all users.

--- src/main/java/com/siuuu/service/UserServiceImpl.java
+++ src/main/java/com/siuuu/service/UserServiceImpl.java
@@ -15,7 +15,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
